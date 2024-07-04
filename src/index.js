9b52729e86884a8bf3d861b1a0eac31781ad46f3
Eva Kerrigan <evakerrigan@gmail.com>
2024-07-04 14:33:45 +0600

    feat: change chart

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import {
   dataWeightJune,
 } from "./data";
 
+import { dataMarch, dataApril, dataMay, dataJune } from "./elseData";
+
 function generateLabels(startDate, endDate) {
   let currentDate = new Date(startDate);
   let end = new Date(endDate);
@@ -23,6 +25,13 @@ function generateLabels(startDate, endDate) {
 const startDate = "2024-03-01";
 const endDate = "2024-06-30";
 const allMonthsLabels = generateLabels(startDate, endDate);
+
+function getMonthIndexByDate(dateString) {
+  const monthNames = ["march", "april", "may", "june"];
+  const date = new Date(dateString);
+  const monthName = monthNames[date.getMonth()];
+  return allMonthsLabels.indexOf(monthName);
+}
 
 new Chart(document.getElementById("myChart"), {
   type: "line",
@@ -68,16 +77,32 @@ new Chart(document.getElementById("myChart"), {
         })),
         // Дополнительные настройки датасета
       },
+    //   {
+    //     type: "bar",
+    //     label: "",
+    //     data: dataMarch.concat(dataApril, dataMay, dataJune).map((date) => ({
+    //       x: date,
+    //       y: 62,
+    //       mode: "markers",
+    //       marker: {
+    //         color: "grey",
+    //       },
+    //     })),
+    //   },
     ],
   },
   options: {
+    layout: {
+      height: 200,
+      width: 2000,
+    },
     scales: {
       y: {
         type: "linear",
         position: "left",
         beginAtZero: true,
-        min: 58,
-        max: 63,
+        min: 59,
+        max: 62,
         ticks: {
           stepSize: 1,
         },
@@ -85,48 +110,3 @@ new Chart(document.getElementById("myChart"), {
     },
   },
 });
-
-// const ctx2 = document.getElementById('myChart').getContext('2d');
-// new Chart(ctx2, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [65, 65, 65, 65, 65, 65, 65],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//     scales: {
-//         y: {
-//             type: 'linear', // Указываем, что хотим использовать линейный масштаб
-//             position: 'left', // Опционально, указывает положение масштаба
-//             beginAtZero: true, // Масштаб начинается с нуля
-//             min: 50, // Минимальное значение
-//             max: 65, // Максимальное значение
-//             ticks: {
-//                 stepSize: 1 // Размер шага между делениями
-//             }
-//         }
-//     }
-// }
-// });
