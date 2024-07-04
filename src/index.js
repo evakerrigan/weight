e1005a5795ef91e68ffc0a00d76c3f00d2aa5aea
Eva Kerrigan <evakerrigan@gmail.com>
2024-07-04 15:13:42 +0600

    feat: process develop

--- src/index.js
+++ src/index.js
@@ -77,25 +77,53 @@ new Chart(document.getElementById("myChart"), {
         })),
         // Дополнительные настройки датасета
       },
-    //   {
-    //     type: "bar",
-    //     label: "",
-    //     data: dataMarch.concat(dataApril, dataMay, dataJune).map((date) => ({
-    //       x: date,
-    //       y: 62,
-    //       mode: "markers",
-    //       marker: {
-    //         color: "grey",
-    //       },
-    //     })),
-    //   },
+      {
+        type: "bar",
+        label: "еще",
+        data: dataMarch.concat(dataApril, dataMay, dataJune).map((date) => ({
+          x: date,
+          y: 62,
+          mode: "markers",
+          marker: {
+            color: "gray",
+          },
+        })),
+      },
+      {
+      type: 'bar', // Изменено с 'line' на 'bar'
+      label: "месяца",
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green'],
+          datasets: [{
+              label: '# of Votes',
+              data: [62, 62, 62, 62, 62, 62],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                //   'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                //   'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                //   'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                //   'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+    },
     ],
   },
   options: {
-    layout: {
-      height: 200,
-      width: 2000,
-    },
+    // layout: {
+    //   height: 200,
+    //   width: 2000,
+    // },
     scales: {
       y: {
         type: "linear",
