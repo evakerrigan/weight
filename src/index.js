5202c57dfd64adb0da82771302f12bfa3abe0c18
Eva Kerrigan <evakerrigan@gmail.com>
2024-07-04 16:09:32 +0600

    feat: done

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import {
   dataWeightApril,
   dataWeightMay,
   dataWeightJune,
+  dataWeightJuly,
 } from "./data";
 
-import { dataMarch, dataApril, dataMay, dataJune } from "./elseData";
+import { dataMarch, dataApril, dataMay, dataJune, dataJuly } from "./elseData";
 
 function generateLabels(startDate, endDate) {
   let currentDate = new Date(startDate);
@@ -23,15 +24,8 @@ function generateLabels(startDate, endDate) {
 }
 
 const startDate = "2024-03-01";
-const endDate = "2024-06-30";
+const endDate = "2024-07-04";
 const allMonthsLabels = generateLabels(startDate, endDate);
-
-function getMonthIndexByDate(dateString) {
-  const monthNames = ["march", "april", "may", "june"];
-  const date = new Date(dateString);
-  const monthName = monthNames[date.getMonth()];
-  return allMonthsLabels.indexOf(monthName);
-}
 
 new Chart(document.getElementById("myChart"), {
   type: "line",
@@ -39,33 +33,42 @@ new Chart(document.getElementById("myChart"), {
     labels: allMonthsLabels,
     datasets: [
       {
-        label: "Вес в марте",
+        label: "МАРТ",
         data: dataWeightMarch.map((value, index) => ({
           x: allMonthsLabels[index],
           y: value,
         })),
-        // Дополнительные настройки датасета
+        fill: true,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 2,
       },
       {
-        label: "Вес в апреле",
+        label: "АПРЕЛЬ",
         data: dataWeightApril.map((value, index) => ({
           x: allMonthsLabels[index + dataWeightMarch.length],
           y: value,
         })),
-        // Дополнительные настройки датасета
+        fill: true,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        borderColor: "rgba(54, 162, 235, 1)",
+        borderWidth: 2,
       },
       {
-        label: "Вес в мае",
+        label: "МАЙ",
         data: dataWeightMay.map((value, index) => ({
           x: allMonthsLabels[
             index + dataWeightMarch.length + dataWeightApril.length
           ],
           y: value,
         })),
-        // Дополнительные настройки датасета
+        fill: true,
+        backgroundColor: "rgba(255, 206, 86, 0.2)",
+        borderColor: "rgba(255, 206, 86, 1)",
+        borderWidth: 2,
       },
       {
-        label: "Вес в июне",
+        label: "ИЮНЬ",
         data: dataWeightJune.map((value, index) => ({
           x: allMonthsLabels[
             index +
@@ -75,12 +78,32 @@ new Chart(document.getElementById("myChart"), {
           ],
           y: value,
         })),
-        // Дополнительные настройки датасета
+        fill: true,
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 2,
+      },
+      {
+        label: "ИЮЛЬ",
+        data: dataWeightJuly.map((value, index) => ({
+          x: allMonthsLabels[
+            index +
+              dataWeightMarch.length +
+              dataWeightApril.length +
+              dataWeightMay.length +
+              dataWeightJune.length
+          ],
+          y: value,
+        })),
+        fill: true,
+        backgroundColor: "rgba(153, 102, 255, 0.2)",
+        borderColor: "rgba(153, 102, 255, 1)",
+        borderWidth: 2,
       },
       {
         type: "bar",
         label: "еще",
-        data: dataMarch.concat(dataApril, dataMay, dataJune).map((date) => ({
+        data: dataMarch.concat(dataApril, dataMay, dataJune, dataJuly).map((date) => ({
           x: date,
           y: 62,
           mode: "markers",
@@ -89,41 +112,9 @@ new Chart(document.getElementById("myChart"), {
           },
         })),
       },
-      {
-      type: 'bar', // Изменено с 'line' на 'bar'
-      label: "месяца",
-      data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green'],
-          datasets: [{
-              label: '# of Votes',
-              data: [62, 62, 62, 62, 62, 62],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                //   'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                //   'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                //   'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                //   'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-    },
     ],
   },
   options: {
-    // layout: {
-    //   height: 200,
-    //   width: 2000,
-    // },
     scales: {
       y: {
         type: "linear",
@@ -138,3 +129,19 @@ new Chart(document.getElementById("myChart"), {
     },
   },
 });
+
+//           backgroundColor: [
+//             "rgba(255, 99, 132, 0.2)",
+//             "rgba(54, 162, 235, 0.2)",
+//             "rgba(255, 206, 86, 0.2)",
+//             'rgba(75, 192, 192, 0.2)',
+//             "rgba(153, 102, 255, 0.2)",
+//             'rgba(255, 159, 64, 0.2)'
+//           ],
+//           borderColor: [
+//             "rgba(255, 99, 132, 1)",
+//             "rgba(54, 162, 235, 1)",
+//             "rgba(255, 206, 86, 1)",
+//             'rgba(75, 192, 192, 1)',
+//             "rgba(153, 102, 255, 1)",
+//             'rgba(255, 159, 64, 1)'
